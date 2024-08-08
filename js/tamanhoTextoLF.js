d779c16dd152e8a5c264973ac482fb536458966d
DANIEL DORIGAN DE CARVALHO CAMPOS <u23124@academico.cotuca.unicamp.br>
2024-08-08 11:42:03 -0300

    terminar responsibilidade dos cards Linguagem e ferramenta

---
+++ js/tamanhoTextoLF.js
@@ -0,0 +1,23 @@
+function adjustFontSize() {
+    const box = document.getElementById('cardsLingFerrBox');
+
+    const cards = box.querySelectorAll(".cardLingFerr")
+
+    cards.forEach(element => {
+        const container = element.querySelector(".lingFerrTitulo");
+        let fontSize = 32; // começa tentando 2rem
+        
+
+        container.style.fontSize = fontSize + 'px';
+        
+        while (container.scrollHeight > container.clientHeight || container.scrollWidth > container.clientWidth) {
+            fontSize--;
+            container.style.fontSize = fontSize + 'px';
+        }
+        console.log(container, fontSize, container.clientWidth)
+    });
+
+}
+
+window.addEventListener('resize', adjustFontSize)
+adjustFontSize(); // Adjust on initial load
