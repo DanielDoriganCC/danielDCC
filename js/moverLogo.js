c2d279502cac1e2c66a04c023485d95c0251db42
Daniel Campos <cc23124@g.unicamp.br>
2024-10-07 08:36:19 -0300

    consertar vários bugs e consertar scroll da tela

--- js/moverLogo.js
+++ js/moverLogo.js
@@ -15,27 +15,28 @@
 //     ultScroll = scroll;
 // });
 
-var ultScroll = 1; // inibe primeira retração, impedindo erro
-const imgMovivel = document.getElementById("logoMovivel");
+document.addEventListener("DOMContentLoaded", () => {
+    var ultScroll = 1; // inibe primeira retração, impedindo erro
+    const imgMovivel = document.getElementById("logoMovivel");
 
-var PosicaoInicial = 50
-var posicao = PosicaoInicial;
-imgMovivel.style.top = `${PosicaoInicial}vh`
+    var PosicaoInicial = 50
+    var posicao = PosicaoInicial;
+    imgMovivel.style.top = `${PosicaoInicial}vh`
 
-var deg = 0
-
-
-window.addEventListener("scroll", () => {
-    const scroll = window.scrollY || document.documentElement.scrollTop;
-
-    posicao = posicao + (scroll - ultScroll) * 0.14   // calcula a posicao
-    
-    if (posicao < 90 + PosicaoInicial ){
-        imgMovivel.style.top = `${ posicao }vh`
-        imgMovivel.style.transform = `rotate( ${ (deg + posicao - PosicaoInicial ) * 4 }deg)` // subtrair posição inicial (50)
-    }
+    var deg = 0
 
 
-    ultScroll = scroll;
-});
+    window.addEventListener("scroll", () => {
+        const scroll = window.scrollY || document.documentElement.scrollTop;
 
+        posicao = posicao + (scroll - ultScroll) * 0.14   // calcula a posicao
+        
+        if (posicao < 90 + PosicaoInicial ){
+            imgMovivel.style.top = `${ posicao }vh`
+            imgMovivel.style.transform = `rotate( ${ (deg + posicao - PosicaoInicial ) * 4 }deg)` // subtrair posição inicial (50)
+        }
+
+
+        ultScroll = scroll;
+    });
+})
